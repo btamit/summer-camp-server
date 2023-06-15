d2869dbf72245e7f38bde4d8948f04a1118b273c
btamit <bishojittoppo118@gmail.com>
2023-06-16 01:35:01 +0600

    jwt & axios implemented

--- index.js
+++ index.js
@@ -55,14 +55,22 @@ async function run() {
       res.send({token})
     })
 
-
+    const verifyAdmin = async(req,res,next)=>{
+      const email = req.decoded.email;
+      const query = {email:email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !=='admin'){
+        return res.status(403).send({error:true,message:'forbidden access'})
+      }
+      next();
+    }
 
 
 
 
 
     // Users API
-    app.get('/users', async (req,res) =>{
+    app.get('/users',verifyJWT,verifyAdmin, async (req,res) =>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -79,6 +87,20 @@ async function run() {
        const result = await usersCollection.insertOne(user);
        res.send(result);
      });
+
+     app.get('/users/admin/:email',verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !==email){
+        res.send({admin:false})
+      }
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      const result = {admin:user?.role ==='admin'}
+      res.send(result);
+     })
+
+
+
 
     app.patch('/users/admin/:id', async(req,res) =>{
       const id = req.params.id;
