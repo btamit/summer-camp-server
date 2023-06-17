99c0347ac99c6efe841dfd96fd305ed50c7b5e1d
btamit <bishojittoppo118@gmail.com>
2023-06-17 07:02:40 +0600

    api for payment added

--- index.js
+++ index.js
@@ -193,8 +193,10 @@ async function run() {
     // Payment APi
     app.post('/payments',verifyJWT, async(req,res)=>{
       const payment = req.body;
-      const result = await paymentCollection.insertOne(payment)
-      res.send(result);
+      const insertResult = await paymentCollection.insertOne(payment);
+      const query = {_id: {$in: payment.cartItems.map(id => new ObjectId(id))}};
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send(insertResult, deleteResult);
     })
 
 
